4cda5d08f2cb7ccbfd8a205fe76256cbc6455854
melon556 <118828659+melon556@users.noreply.github.com>
2026-08-06 16:58:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const CENSOR_DURATION_MS = window.matchMedia(
   "(prefers-reduced-motion: reduce)",
 ).matches
   ? 0
-  : 2000;
+  : 1000;
 let censorTimeoutId = null;
 
 function render(censorDuringFlip) {
